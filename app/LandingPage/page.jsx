75edcc3c3bd19751d539f709135e734977f7ceb9
Ravle <ravle@gmail.com>
2025-12-16 21:09:41 +0530

    add repo

---
+++ app/LandingPage/page.jsx
@@ -0,0 +1,11 @@
+import {Navbar1} from "@/components/ui/navbar1"
+export default  function LandingPage(){
+    
+    return(
+        <>
+        <Navbar1/>
+        <Herosection/>
+        <Collectivesection/>
+        </>
+    )
+}
---
+++ app/page.js
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import LandingPage from "./LandingPage/page"
+import About from "./About/page"
+import Program from "./Program/page"
+
+export default function Home() {
+  return (
+    <div>
+      <LandingPage/>
+      <Program/>
+      <About/>
+    </div>
+  );
+}
